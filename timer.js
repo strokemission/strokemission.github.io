c966b14c3d88a3a985426edac6a4e9f593660ba7
FurnaceTaken <78284696+FurnaceTaken@users.noreply.github.com>
2021-01-31 01:54:59 +0100

    Initial commit

---
+++ timer.js
@@ -0,0 +1,25 @@
+
+// Set the date we're counting up to
+var countDownDate = new Date("jun 11, 2019 12:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = now - countDownDate;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("day").innerHTML = days 
+  document.getElementById("hour").innerHTML = hours 
+  document.getElementById("minute").innerHTML = minutes 
+  document.getElementById("second").innerHTML = seconds
+}, 1000);
